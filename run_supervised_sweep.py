df608b3b408d87ff5c4bc0dc5f6c3a017de269ef
SebastianHafner <hafnersailing@gmail.com>
2023-02-28 12:28:45 +0100

    yes

--- run_supervised_sweep.py
+++ run_supervised_sweep.py
@@ -28,99 +28,99 @@ if __name__ == '__main__':
         with wandb.init(config=sweep_cfg):
             sweep_cfg = wandb.config
 
-        net = networks.PopulationNet(cfg.MODEL)
-        net.to(device)
-        optimizer = optim.AdamW(net.parameters(), lr=sweep_cfg.lr, weight_decay=0.01)
-        criterion = loss_functions.get_criterion(cfg.MODEL.LOSS_TYPE)
+            net = networks.PopulationNet(cfg.MODEL)
+            net.to(device)
+            optimizer = optim.AdamW(net.parameters(), lr=sweep_cfg.lr, weight_decay=0.01)
+            criterion = loss_functions.get_criterion(cfg.MODEL.LOSS_TYPE)
 
-        # reset the generators
-        dataset = datasets.PopDataset(cfg=cfg, run_type='train')
-        print(dataset)
+            # reset the generators
+            dataset = datasets.PopDataset(cfg=cfg, run_type='train')
+            print(dataset)
 
-        dataloader_kwargs = {
-            'batch_size': sweep_cfg.batch_size,
-            'num_workers': 0 if cfg.DEBUG else cfg.DATALOADER.NUM_WORKER,
-            'shuffle': cfg.DATALOADER.SHUFFLE,
-            'drop_last': True,
-            'pin_memory': True,
-        }
-        dataloader = torch_data.DataLoader(dataset, **dataloader_kwargs)
+            dataloader_kwargs = {
+                'batch_size': sweep_cfg.batch_size,
+                'num_workers': 0 if cfg.DEBUG else cfg.DATALOADER.NUM_WORKER,
+                'shuffle': cfg.DATALOADER.SHUFFLE,
+                'drop_last': True,
+                'pin_memory': True,
+            }
+            dataloader = torch_data.DataLoader(dataset, **dataloader_kwargs)
 
-        # unpacking cfg
-        epochs = cfg.TRAINER.EPOCHS
-        steps_per_epoch = len(dataloader)
+            # unpacking cfg
+            epochs = cfg.TRAINER.EPOCHS
+            steps_per_epoch = len(dataloader)
 
-        # tracking variables
-        global_step = epoch_float = 0
+            # tracking variables
+            global_step = epoch_float = 0
 
-        # early stopping
-        best_rmse_val, trigger_times = 0, 0
-        stop_training = False
+            # early stopping
+            best_rmse_val, trigger_times = 0, 0
+            stop_training = False
 
-        for epoch in range(1, epochs + 1):
-            print(f'Starting epoch {epoch}/{epochs}.')
+            for epoch in range(1, epochs + 1):
+                print(f'Starting epoch {epoch}/{epochs}.')
 
-            start = timeit.default_timer()
-            loss_set, pop_set = [], []
+                start = timeit.default_timer()
+                loss_set, pop_set = [], []
 
-            for i, batch in enumerate(dataloader):
+                for i, batch in enumerate(dataloader):
 
-                net.train()
-                optimizer.zero_grad()
+                    net.train()
+                    optimizer.zero_grad()
 
-                x = batch['x'].to(device)
-                y_gts = batch['y'].to(device)
-                y_pred = net(x)
+                    x = batch['x'].to(device)
+                    y_gts = batch['y'].to(device)
+                    y_pred = net(x)
 
-                loss = criterion(y_pred, y_gts.float())
-                loss.backward()
-                optimizer.step()
+                    loss = criterion(y_pred, y_gts.float())
+                    loss.backward()
+                    optimizer.step()
 
-                loss_set.append(loss.item())
-                pop_set.append(y_gts.flatten())
+                    loss_set.append(loss.item())
+                    pop_set.append(y_gts.flatten())
 
-                global_step += 1
-                epoch_float = global_step / steps_per_epoch
+                    global_step += 1
+                    epoch_float = global_step / steps_per_epoch
 
-                if global_step % cfg.LOGGING.FREQUENCY == 0:
-                    # print(f'Logging step {global_step} (epoch {epoch_float:.2f}).')
-                    time = timeit.default_timer() - start
+                    if global_step % cfg.LOGGING.FREQUENCY == 0:
+                        # print(f'Logging step {global_step} (epoch {epoch_float:.2f}).')
+                        time = timeit.default_timer() - start
+                        wandb.log({
+                            'loss': np.mean(loss_set),
+                            'time': time,
+                            'step': global_step,
+                            'epoch': epoch_float,
+                        })
+                        start = timeit.default_timer()
+                        loss_set = []
+                    # end of batch
+
+                assert (epoch == epoch_float)
+                print(f'epoch float {epoch_float} (step {global_step}) - epoch {epoch}')
+                # evaluation at the end of an epoch
+                _ = evaluation.model_evaluation(net, cfg, 'train', epoch_float, global_step)
+                rmse_val = evaluation.model_evaluation(net, cfg, 'val', epoch_float, global_step)
+
+                if rmse_val >= best_rmse_val:
+                    trigger_times += 1
+                    if trigger_times > cfg.TRAINER.PATIENCE:
+                        stop_training = True
+                else:
+                    best_rmse_val = rmse_val
                     wandb.log({
-                        'loss': np.mean(loss_set),
-                        'time': time,
+                        'best val rmse': best_rmse_val,
                         'step': global_step,
                         'epoch': epoch_float,
                     })
-                    start = timeit.default_timer()
-                    loss_set = []
-                # end of batch
+                    print(f'saving network (F1 {rmse_val:.3f})', flush=True)
+                    networks.save_checkpoint(net, optimizer, epoch, cfg)
+                    trigger_times = 0
 
-            assert (epoch == epoch_float)
-            print(f'epoch float {epoch_float} (step {global_step}) - epoch {epoch}')
-            # evaluation at the end of an epoch
-            _ = evaluation.model_evaluation(net, cfg, 'train', epoch_float, global_step)
-            rmse_val = evaluation.model_evaluation(net, cfg, 'val', epoch_float, global_step)
+                if stop_training:
+                    break  # end of training by early stopping
 
-            if rmse_val >= best_rmse_val:
-                trigger_times += 1
-                if trigger_times > cfg.TRAINER.PATIENCE:
-                    stop_training = True
-            else:
-                best_rmse_val = rmse_val
-                wandb.log({
-                    'best val rmse': best_rmse_val,
-                    'step': global_step,
-                    'epoch': epoch_float,
-                })
-                print(f'saving network (F1 {rmse_val:.3f})', flush=True)
-                networks.save_checkpoint(net, optimizer, epoch, cfg)
-                trigger_times = 0
-
-            if stop_training:
-                break  # end of training by early stopping
-
-        net, *_ = networks.load_checkpoint(cfg, device)
-        _ = evaluation.model_evaluation(net, cfg, 'test', epoch_float, global_step)
+            net, *_ = networks.load_checkpoint(cfg, device)
+            _ = evaluation.model_evaluation(net, cfg, 'test', epoch_float, global_step)
 
 
     if args.sweep_id is None:
