b5f59059cfeda002018f260cfb3b0305c7de9b97
SebastianHafner <hafnersailing@gmail.com>
2023-06-08 17:25:46 +0200

    fix

--- run_supervised_sweep.py
+++ run_supervised_sweep.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
             criterion = loss_functions.get_criterion(cfg.MODEL.LOSS_TYPE)
 
             # reset the generators
-            dataset = datasets.PopDataset(cfg=cfg, fold=cfg.DATALOADER.TRAIN)
+            dataset = datasets.PopDataset(cfg=cfg, run_type='train')
             print(dataset)
 
             dataloader_kwargs = {
@@ -97,8 +97,8 @@ if __name__ == '__main__':
                 assert (epoch == epoch_float)
                 print(f'epoch float {epoch_float} (step {global_step}) - epoch {epoch}')
                 # evaluation at the end of an epoch
-                _ = evaluation.model_evaluation(net, cfg, cfg.DATALOADER.TRAIN, epoch_float, global_step)
-                rmse_val = evaluation.model_evaluation(net, cfg, cfg.DATALOADER.VAL, epoch_float, global_step)
+                _ = evaluation.model_evaluation(net, cfg, 'train', epoch_float, global_step)
+                rmse_val = evaluation.model_evaluation(net, cfg, 'val', epoch_float, global_step)
 
                 if best_rmse_val is None or rmse_val < best_rmse_val:
                     best_rmse_val = rmse_val
@@ -119,7 +119,7 @@ if __name__ == '__main__':
                     break  # end of training by early stopping
 
             net, *_ = networks.load_checkpoint(cfg, device)
-            _ = evaluation.model_evaluation(net, cfg, cfg.DATALOADER.TEST, epoch_float, global_step)
+            _ = evaluation.model_evaluation(net, cfg, 'test', epoch_float, global_step)
 
 
     if args.sweep_id is None:
